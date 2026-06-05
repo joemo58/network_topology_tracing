2d0d3d80073b0df1ce0e19508cdb33f540647479
Joe McGirr <joemcgirr89@gmail.com>
2026-06-05 15:39:24 +1000

    First pass on the data models

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 # Running The App 
 
 # Assumptions
+- When Site deleted, delete all devices associated with it as we won't need them anymore
 
 # High-Level Design
--- api/models.py
+++ api/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Site(models.Model):
+    class Status(models.TextChoices):
+        ACTIVE = 'AC' 'ACTIVE'
+        PLANNED = 'PL' 'PLANNED'
+        DECOMMISSIONED = 'DE' 'DECOMISSIONED'
+
+    name = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    description = models.CharField(max_length=70, blank=True, default="")
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.ACTIVE)
+
+
+class Device(models.Model):
+    name = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    site = models.ForeignKey(Site, on_delete=models.CASCADE)
+    serial_num = models.CharField(max_length=50, unique=True)
+
+
+class Interface(models.Model):
+    class Status(models.TextChoices):
+        UP = 'UP' 'UP'
+        DOWN = 'DO' 'DOWN'
+        MAINTENANCE = 'MA' 'MAINTENANCE'
+
+    name = models.CharField()
+    device = models.ForeignKey(Device, on_delete=models.CASCADE)
+    speed = models.IntegerField()
+    status = models.CharField(max_length=2, choices=Status.choices)
+
+    class META:
+        constraints = [
+            models.UniqueConstraint(
+                name="unique_name",
+                fields=["name", "device"]
+            )
+        ]
+
+
+class Connection(models.Model):
+    class Status(models.TextChoices):
+        CONNECTED = 'CON' 'CONNECTED'
+        DISCONNECTED = 'DIS' 'DISCONNECTED'
+    connection_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    name = models.CharField(max_length=70, blank=True, default="")
+    status = models.CharField(max_length=3, choices=Status.choices)
+    # prevent deleting Devices/Interfaces while Interface is part of a connection
+    start_interface = models.ForeignKey(Interface, on_delete=models.PROTECT)
+    end_interface = models.ForeignKey(Interface, on_delete=models.PROTECT)
