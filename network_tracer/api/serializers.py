daa4941b16f8521849bcef74bc52fbb10371df6f
Joe McGirr <joemcgirr89@gmail.com>
2026-06-05 17:11:36 +1000

    Use HyperlinkedModelSerialisers for easier nav. Updated connectionSerialiser to take {site, device, interface} tuple

--- network_tracer/api/serializers.py
+++ network_tracer/api/serializers.py
@@ -1,27 +1,64 @@
 from rest_framework import serializers
 from api.models import Connection, Device, Interface, Site
 
-class SiteSerializer(serializers.ModelSerializer):
+class SiteSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Site
-        fields = ["name", "description", "status"]
+        fields = ["url", "name", "description", "status"]
 
 
-class DeviceSerializer(serializers.ModelSerializer):
+class DeviceSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Device
-        fields = ["name", "site", "serial_num"]
+        fields = ["url", "name", "site", "serial_num"]
 
 
-class InterfaceSerializer(serializers.ModelSerializer):
+class InterfaceSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Interface
-        fields = ["name", "device", "speed", "status"]
+        fields = ["url", "name", "device", "speed", "status"]
 
 
-class ConnectionSerializer(serializers.ModelSerializer):
+class ConnectionEndpointSerializer(serializers.Serializer):
+    site = serializers.CharField()
+    device = serializers.CharField()
+    interface = serializers.CharField()
+
+    def validate(self, data):
+        try:
+            interface = Interface.objects.get(
+                name=data["interface"],
+                device__name=data["device"],
+                device__site__name=data["site"],
+            )
+        except Interface.DoesNotExist:
+            raise serializers.ValidationError(
+                f"No interface '{data['interface']}' on device '{data['device']}' at site '{data['site']}'"
+            )
+        data["_resolved"] = interface
+        return data
+
+
+class ConnectionSerializer(serializers.HyperlinkedModelSerializer):
+    start = ConnectionEndpointSerializer(write_only=True)
+    end = ConnectionEndpointSerializer(write_only=True)
+
     class Meta:
         model = Connection
-        fields = ["connection_id", "name", "status", 
-                  "start_interface", "end_interface"] 
+        fields = ["url", "connection_id", "name", "status", "start", "end",
+                  "start_interface", "end_interface"]
+        read_only_fields = ["connection_id", "start_interface", "end_interface"]
 
+    def create(self, validated_data):
+        start_iface = validated_data.pop("start")["_resolved"]
+        end_iface = validated_data.pop("end")["_resolved"]
+        return Connection.objects.create(
+            start_interface=start_iface, end_interface=end_iface, **validated_data
+        )
+
+    def update(self, instance, validated_data):
+        if "start" in validated_data:
+            instance.start_interface = validated_data.pop("start")["_resolved"]
+        if "end" in validated_data:
+            instance.end_interface = validated_data.pop("end")["_resolved"]
+        return super().update(instance, validated_data)
